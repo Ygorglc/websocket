a46173c456aef0a8f52b193846bc45a8a60a38aa
ygorglc <ygorglc@gmail.com>
2022-09-19 10:29:39 -0300

    primeiro commit

---
+++ src/test/java/com/ygor/websocket/SpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygor.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
